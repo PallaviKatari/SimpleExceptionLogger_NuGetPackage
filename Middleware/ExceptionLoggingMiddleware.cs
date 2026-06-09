086b6b4f36dc2e0fb66753bc82c3e27696d8446b
PallaviKatari <pallavikatari@gmail.com>
2026-06-09 19:04:25 +0530

    commit_9.6.26

--- Middleware/ExceptionLoggingMiddleware.cs
+++ Middleware/ExceptionLoggingMiddleware.cs
@@ -6,6 +6,14 @@ using Microsoft.Extensions.Logging;
 
 namespace SimpleExceptionLogger.Middleware
 {
+    /// <summary>
+    /// STEPS - Testing Version 1.0.2
+    /// Create an API Key from https://www.nuget.org/
+    /// dotnet build -c Release
+    /// dotnet pack -c Release
+    /// API KEY - oy2bnq6fjlvmerip7hcag7wgpvfu7qfepdr5nffqiy33rm
+    /// dotnet nuget push "C:\Pallavi_Koenig\Full Stack React Application Development with .NET 8\React Lab\SimpleExceptionLogger_NuGetPackage\bin\Release\SimpleExceptionLogger.1.0.2.nupkg" --api-key oy2bnq6fjlvmerip7hcag7wgpvfu7qfepdr5nffqiy33rm --source https://api.nuget.org/v3/index.json
+    /// </summary>
     public class ExceptionLoggingMiddleware
     {
         private readonly RequestDelegate _next;
